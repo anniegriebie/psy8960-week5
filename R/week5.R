2f83168fd7ef9ae6907a354599230369dde79127
anniegriebie <GRIEB046@umn.edu>
2023-02-17 14:33:07 -0600

    Fixing formating of comments made previously

--- R/week5.R
+++ R/week5.R
@@ -3,13 +3,13 @@ setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 library(tidyverse)
 library(lubridate)
 
-#Data Import
+# Data Import
 Adata_tbl <- read_delim("../data/Aparticipants.dat", delim = "-", col_names = c("casenum", "parnum", "stimver", "datadate", "qs"))
 Anotes_tbl <- read_csv("../data/Anotes.csv")
 Bdata_tbl <- read_tsv(file = "../data/Bparticipants.dat", col_names = c("casenum", "parnum", "stimver", "datadate", paste0("q", 1:10)))
 Bnotes_tbl <- read_tsv("../data/Bnotes.txt")
 
-#Data Cleaning
+# Data Cleaning
 Aclean_tbl <- Adata_tbl %>%
   separate(qs, paste0("q",1:5), sep = " - ")  %>%
   mutate(datadate = mdy_hms(datadate)) %>%
