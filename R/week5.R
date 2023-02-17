d1771d080a14652376ed67ba4f5fc639cdbe369e
anniegriebie <GRIEB046@umn.edu>
2023-02-17 14:18:40 -0600

    First data import

--- R/week5.R
+++ R/week5.R
@@ -2,3 +2,7 @@
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 library(tidyverse)
 library(lubridate)
+
+#Data Import
+Adata_tbl <- read_delim("../data/Aparticipants.dat", delim = "-", col_names = c("casenum", "parnum", "stimver", "datadate", "qs"))
+Anotes_tbl <- read_csv("../data/Anotes.csv")
